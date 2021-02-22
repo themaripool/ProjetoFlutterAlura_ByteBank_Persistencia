705fc79827a31a24e8609cfacede87947d2cbb8f
MariAndrade <marielama@outlook.com.br>
2021-02-22 20:39:58 -0300

    [ADD] FutureBuilder and loading on list

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,25 +1,51 @@
 import 'package:bytebank/Models/contacts.dart';
+import 'package:bytebank/database/app_database.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactList extends StatelessWidget {
-
-  final List<Contact> contacts = List();
-
   @override
   Widget build(BuildContext context) {
-    contacts.add(Contact(1, 'marielinha', 1000));
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista de contatos"),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Contact contact = contacts[index];
-          return _ContactItem(contact);
+      body: FutureBuilder<List<Contact>>(
+        //primero executa esse future e assim que receber resposta
+        // executa o codigo do builder aonde esta o callback
+        //snapshot -> contem todas as informacoes do future acima dele
+        initialData: List(),
+        future: findAll(),
+        builder: (context, snapshot) {
+          //snapshot.connectionState) -> indica atual estado do snapshot
+          switch (snapshot.connectionState) {
+            case ConnectionState.none: //future ainda n executado
+              break;
+            case ConnectionState.waiting: //verificando se future esta carregando
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [CircularProgressIndicator(), Text("carregando")],
+                ),
+              );
+              break;
+            case ConnectionState.active: //tem dado disponivel mas future n acabou
+              // carregamento por partes
+              break;
+            case ConnectionState.done: // future acabou
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+              break;
+          }
+          return Text("Erro desconhecido");//nao sera executado pq o snapshot ja tem todos os casos possiveis
         },
-        itemCount: contacts.length,
-        
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -38,7 +64,6 @@ class ContactList extends StatelessWidget {
 }
 
 class _ContactItem extends StatelessWidget {
-
   final Contact contact;
   _ContactItem(this.contact);
 
@@ -48,11 +73,15 @@ class _ContactItem extends StatelessWidget {
       child: ListTile(
         title: Text(
           contact.name,
-          style: TextStyle(fontSize: 24.0,),
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
         ),
         subtitle: Text(
           contact.account_number.toString(),
-          style: TextStyle(fontSize: 16.0,),
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
         ),
       ),
     );
